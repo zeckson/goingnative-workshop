50d36278db234d9ccdcdf041c67436f91d3e3c87
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-01-28 16:27:46 +0300

    task #6 done

--- myaddon/index.js
+++ myaddon/index.js
@@ -1,4 +1,4 @@
 const bindings = require('bindings');
 const addon = bindings('myaddon');
 
-addon.print(process.argv[2]);
+console.log(addon.length(process.argv[2]));
--- myaddon/myaddon.cc
+++ myaddon/myaddon.cc
@@ -12,9 +12,27 @@ void Print(const Nan::FunctionCallbackInfo<Value>& info) {
   printf("%s\n", *String::Utf8Value(str.ToLocalChecked()));
 }
 
+void Length(const Nan::FunctionCallbackInfo<Value>& info) {
+  Nan::MaybeLocal<String> str = Nan::To<String>(info[0]);
+  if (str.IsEmpty()) {
+    Nan::ThrowTypeError("Wrong arguments");
+    return;
+  }
+
+  int length = strlen(*String::Utf8Value(str.ToLocalChecked()));
+
+//  printf("%d\n", length);
+
+  Local<Number> num = Nan::New(length);
+
+  info.GetReturnValue().Set(num);
+}
+
 void Init(Local<Object> exports) {
   exports->Set(Nan::New("print").ToLocalChecked(),
                Nan::New<FunctionTemplate>(Print)->GetFunction());
+  exports->Set(Nan::New("length").ToLocalChecked(),
+               Nan::New<FunctionTemplate>(Length)->GetFunction());
 }
 
 NODE_MODULE(myaddon, Init)
